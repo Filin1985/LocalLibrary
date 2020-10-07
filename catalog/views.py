336715dd36e2df2506a507d0e5593abb36035d84
Марат <ihsmen@iMac-Marat.local>
2020-10-07 18:26:30 +0300

    'create the firts view and templates'

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+
+    num_instance_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    return render(request, 'index.html', context={'num_books':num_books, 'num_instance':num_instance, 'num_instance_available':num_instance_available, 'num_authors':num_authors})
+
+    
