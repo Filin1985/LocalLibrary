326e34e7db882bb6db05bd2d42f3348d0272968d
Марат <ihsmen@iMac-Marat.local>
2020-10-09 11:49:59 +0300

    'created all books view and book detail view'

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -12,4 +13,18 @@ def index(request):
 
     return render(request, 'index.html', context={'num_books':num_books, 'num_instance':num_instance, 'num_instance_available':num_instance_available, 'num_authors':num_authors})
 
-    
+class BookListView(generic.ListView):
+    model = Book
+    # context_object_name = 'my_book_list'
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
