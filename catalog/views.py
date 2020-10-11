2fea8c927d70ab33d977a06ab317ca08ca68ae86
Марат <ihsmen@iMac-Marat.local>
2020-10-11 10:21:28 +0300

    first commit

--- catalog/views.py
+++ catalog/views.py
@@ -2,16 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 
 # Create your views here.
-from .models import Book, Author, BookInstance, Genre
+from .models import Book, Genre
 
 def index(request):
     num_books = Book.objects.all().count()
-    num_instance = BookInstance.objects.all().count()
 
-    num_instance_available = BookInstance.objects.filter(status__exact='a').count()
-    num_authors = Author.objects.count()
-
-    return render(request, 'index.html', context={'num_books':num_books, 'num_instance':num_instance, 'num_instance_available':num_instance_available, 'num_authors':num_authors})
+    return render(request, 'index.html', context={'num_books':num_books})
 
 class BookListView(generic.ListView):
     model = Book
